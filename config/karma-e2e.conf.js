6ef1bd93bedd40a66133083d6fd8da1260190d3a
Aaron Holmes <aholmes@pureguru-software.com>
2014-01-24 20:44:56 +0000

    using phantomjs instead of chrome

--- config/karma-e2e.conf.js
+++ config/karma-e2e.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config){
 
     autoWatch : false,
 
-    browsers : ['Chrome'],
+    browsers : ['PhantomJS'],
 
     frameworks: ['ng-scenario'],
 
@@ -25,7 +25,8 @@ module.exports = function(config){
             'karma-chrome-launcher',
             'karma-firefox-launcher',
             'karma-jasmine',
-            'karma-ng-scenario'
+            'karma-ng-scenario',
+						'karma-phantomjs-launcher'
             ],
 
     junitReporter : {
