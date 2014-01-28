024c70a30be7d9664e6cc8ace734880832a02008
Aaron Holmes <aholmes@pureguru-software.com>
2014-01-28 19:29:04 +0000

    Adding events to eventDetails

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -11,7 +11,39 @@ eventsApp.controller('EventController',
 					city: "Ottawa",
 					province: "Ontario"
 				},
-				imageUrl: "http://devpnt.com/wp-content/uploads/2013/08/angularjs-logo.png.pagespeed.ce_.2SfPGmgT_b.png"
-			}	
+				imageUrl: "http://devpnt.com/wp-content/uploads/2013/08/angularjs-logo.png.pagespeed.ce_.2SfPGmgT_b.png",
+				sessions: [
+					{
+						name: "Welcoming Speech",
+						creatorName: "Aaron Holmes",
+						duration: "2 hours",
+						abstract:	"I will be giving a great welcoming speech.",
+						upVoteCount: 0
+					},
+					{
+						name: "Dinner",
+						creatorName: "Ashy",
+						duration:	"5 Minutes",
+						abstract: "We will be dining on poo and kibbles. We will eat as fast as we can and then beg for more!",
+						upVoteCount: 0
+					},
+					{	
+						name: "Dancing",
+						creatorName: "Jaxon",
+						duration: "3 hours",
+						abstract: "There is something wrong with me.",
+						upVoteCount: 0
+					}
+				]
+			}
+			
+		$scope.upVoteSession = function(session) {
+			session.upVoteCount++;
+		}	
+
+		$scope.downVoteSession = function(session) {
+			session.upVoteCount--;
+		}
+
 		}
 );
